42b466fc2d12adc2ae8d5252c41bc19da49a75dc
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-07 03:37:48 +0200

    feat: added solution for part 2

--- src/bin/part2/main.rs
+++ src/bin/part2/main.rs
@@ -1,3 +1,35 @@
+use aoc_2024_day2::{puzzle_input::PuzzleInput, report::Report};
+
 fn main() {
     println!("solving part 2...");
+
+    let input_string = include_str!("../../../input/numbers.txt");
+    let input = PuzzleInput::from(input_string);
+
+    println!("{}", input);
+
+    let mut sum = 0;
+    for item in input.reports() {
+        let levels = item.levels();
+        let mut safe = false;
+
+        println!("analizing: {}", item);
+        for i in 0..levels.len() {
+            let mut levels = levels.clone();
+            levels.remove(i);
+
+            let report = Report(levels);
+            if report.is_safe() { safe = true };
+
+            println!("  {} {}", report, safe);
+        }
+
+        println!("verdict: {}\n", safe);
+        match safe {
+            true => sum += 1,
+            false => (),
+        }
+    }
+
+    println!("{}", sum);
 }
